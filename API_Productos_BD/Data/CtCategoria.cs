7bcb750784fa334d411e658b7811c019f8b5ebfe
Pilar Najera <pnajera@casystem.com.mx>
2020-09-21 19:52:33 -0500

    Primera versión
    API productos

---
+++ API_Productos_BD/Data/CtCategoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data
+{
+    public partial class CtCategoria
+    {
+        public CtCategoria()
+        {
+            CtProducto = new HashSet<CtProducto>();
+        }
+
+        public long PKIdCategoria { get; set; }
+        public string Descripcion { get; set; }
+        public Boolean Activo { get; set; }
+
+        public virtual ICollection<CtProducto> CtProducto { get; set; }
+    }
+}
---
+++ API_Productos_BD/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Entities
+{
+    public class Categoria
+    {
+        public long IdCategoria { get; set; }
+        public string Descripcion { get; set; }
+        public Boolean Activo { get; set; }
+    }
+}
